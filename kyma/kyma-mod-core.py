3cdb09fd99e7cbff9af7955b0ee63bc65d748834
赤い計算機 <98051617+akai-keisanki@users.noreply.github.com>
2023-12-08 12:26:54 -0300

    Create kyma-mod-core.py

---
+++ kyma/kyma-mod-core.py
@@ -0,0 +1,24 @@
+# Kyma Mod Core Package For LuxOS 1.0 ( Unfinished )
+# Kyma's Modification Core For LuxOS 1.0 And Main Scripts
+
+class lux_pack:
+  name="kyma-mod-core"
+  version="1.0"
+  aditionaldata={"datalist"=[None]}
+  def startscript():
+    print("\n--- > Κύμα < ---\n")
+  def pexcm(cmd, cliv):
+    splitcmd=cmd.split(" ")
+    r=1
+    if splitcmd[0] == "kyma":
+      try:
+        if self.splitcmd[1] == "#version":
+          print("Κύμα 1.0 - \" Κόσμος\"")
+      except:
+          print(cmd+" >> kyma failed."))
+    else:
+      r=0
+    return r;
+  class packvars:
+    kyma_name="Κύμα"
+    kyma_codename="Κόσμος"

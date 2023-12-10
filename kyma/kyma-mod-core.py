b9440c7be5ea794e511b93dfe2427f0d675d4cf9
赤い計算機 <98051617+akai-keisanki@users.noreply.github.com>
2023-12-10 09:23:32 -0300

    Update kyma-mod-core.py

--- kyma/kyma-mod-core.py
+++ kyma/kyma-mod-core.py
@@ -10,10 +10,22 @@ class lux_pack:
   def pexcm(cmd, cliv):
     splitcmd=cmd.split(" ")
     r=1
-    if splitcmd[0] == "kyma":
+    if splitcmd[0] == "kyma" or splitcmd[0] == "kyma-kosmos":
       try:
         if self.splitcmd[1] == "#version":
           print("Κύμα 1.0 - \" Κόσμος\"")
+        elif self.splitcmd[1] == "update":
+          print("Κύμα 1.0 - \" Κόσμος\"")
+          print("Updating to: Unknown")
+          print("The updating system is broken")
+      except:
+          print(cmd+" >> kyma failed."))
+    elif splitcmd[0] == "kosmos" or splitcmd[0] == "kosmos-kosmos":
+      try:
+        if self.splitcmd[1] == "#version":
+          print("Κόσμος 1.0 - \" Κόσμος\"")
+        elif self.splitcmd[1] == "":
+          print("Κόσμος 1.0 - \" Κόσμος\"")
       except:
           print(cmd+" >> kyma failed."))
     else:
